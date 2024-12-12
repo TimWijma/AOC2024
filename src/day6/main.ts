60e41a1a564eb3e53675953b36ea7801a9db233b
Tim Wijma <tim.wijma@gmail.com>
2024-12-12 16:55:46 +0100

    day6 part 2

--- src/day6/main.ts
+++ src/day6/main.ts
@@ -3,10 +3,42 @@ import { readFile, printResult } from "../../helpers";
 const input = readFile("input.txt");
 
 const map = input.map((line) => line.split(""));
+console.log(map);
+
+const createMap = () => {
+    let inputCopy = [...input];
+    return inputCopy.map((line) => line.split(""));
+};
+
+const outOfBounds = (x: number, y: number) => {
+    return y < 0 || y > map.length - 1 || x < 0 || x > map[y].length - 1;
+};
+
+const turn = (
+    x: number,
+    y: number,
+    xDir: number,
+    yDir: number,
+) => {
+    const directions = [0, 1, 0, -1];
+
+    let newXDir = (xDir + 1) % 4;
+    let newYDir = (yDir + 1) % 4;
+    let newX = x + directions[newXDir];
+    let newY = y + directions[newYDir];
+
+    return [newX, newY, newXDir, newYDir];
+};
+
+const guardY = map.findIndex((row) => row.includes("^"));
+const guardX = map[guardY].indexOf("^");
+map[guardY][guardX] = "#";
 
 const part1 = () => {
-    let currentY = map.findIndex((row) => row.includes("^"));
-    let currentX = map[currentY].indexOf("^");
+    let map = createMap();
+
+    let currentY = guardY;
+    let currentX = guardX;
 
     let directions = [0, 1, 0, -1];
     let xIndex = 0;
@@ -19,15 +51,18 @@ const part1 = () => {
 
         if (outOfBounds(tempX, tempY)) break;
 
-        if (map[tempY][tempX] === "#") {
-            xIndex = (xIndex + 1) % 4;
-            yIndex = (yIndex + 1) % 4;
-            currentX += directions[xIndex];
-            currentY += directions[yIndex];
-        } else {
-            currentX = tempX;
-            currentY = tempY;
+        while (map[tempY][tempX] === "#") {
+            [tempX, tempY, xIndex, yIndex] = turn(
+                currentX,
+                currentY,
+                xIndex,
+                yIndex,
+                true
+            );
         }
+
+        currentX = tempX;
+        currentY = tempY;
     }
 
     return map
@@ -35,8 +70,69 @@ const part1 = () => {
         .reduce((ps, a) => ps + a, 0);
 };
 
-const outOfBounds = (x: number, y: number) => {
-    return y < 0 || y > map.length - 1 || x < 0 || x > map[y].length - 1;
+const part2 = () => {
+    let loops = 0;
+
+    for (let y = 0; y < map.length; y++) {
+        for (let x = 0; x < map[y].length; x++) {
+            let map = createMap();
+
+            if (map[y][x] === "#") continue;
+            map[y][x] = "#";
+
+            let currentY = guardY;
+            let currentX = guardX;
+
+            let directions = [0, 1, 0, -1];
+            let xIndex = 0;
+            let yIndex = 3;
+            let count = 0;
+            let visited: Set<String> = new Set();
+            outerLoop: while (!outOfBounds(currentX, currentY)) {
+                // map[currentY][currentX] = count.toString();
+                map[currentY][currentX] = "X";
+                count++;
+
+                let tempX = currentX + directions[xIndex];
+                let tempY = currentY + directions[yIndex];
+
+                if (y === 8 && x == 7) {
+                    // console.log("e");
+                }
+
+                if (outOfBounds(tempX, tempY)) break;
+
+                while (map[tempY][tempX] === "#") {
+                    [tempX, tempY, xIndex, yIndex] = turn(
+                        currentX,
+                        currentY,
+                        xIndex,
+                        yIndex,
+                        true
+                    );
+
+                    // console.log(x, y);
+                    if (x === 3 && y === 6 ) {
+                        console.log("e");
+                        
+                    }
+
+                    let positionIndex = `${tempX},${tempY},${xIndex},${yIndex}`;
+                    if (visited.has(positionIndex)) {
+                        loops++;
+
+                        break outerLoop;
+                    }
+                    visited.add(positionIndex);
+                }
+
+                currentX = tempX;
+                currentY = tempY;
+            }
+        }
+    }
+
+    return loops;
 };
 
-printResult(part1);
+printResult(part1, part2);
